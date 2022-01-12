90a59ab68c213d6cfe21a5c489954918f3f281a8
Walachooo <j.walach@protonmail.com>
2022-01-12 16:56:21 +0100

    css bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 class App extends React.Component {
@@ -13,28 +13,28 @@ class App extends React.Component {
     })
   }
   render() { 
-    const btn_off = {
-      border: '2px solid green',
-      padding: '10px 20px',
-      fontFamily : 'arial',
-      display:'block',
-      margin:'20px auto',
-      backgroundColor: 'white',
-      color: 'blue'
-    }
-    const btn_on = {
-      border: '4px solid blue',
-      padding: '10px 20px',
-      fontFamily : 'arial',
-      display:'block',
-      margin:'20px auto',
-      backgroundColor: 'red'
-    }
+    // const btn_off = {
+    //   border: '2px solid green',
+    //   padding: '10px 20px',
+    //   fontFamily : 'arial',
+    //   display:'block',
+    //   margin:'20px auto',
+    //   backgroundColor: 'white',
+    //   color: 'blue'
+    // }
+    // const btn_on = {
+    //   border: '4px solid blue',
+    //   padding: '10px 20px',
+    //   fontFamily : 'arial',
+    //   display:'block',
+    //   margin:'20px auto',
+    //   backgroundColor: 'red'
+    // }
     
     return (
     <div>
       <button 
-      style={this.state.active? btn_on:btn_off}
+      className={this.state.active? 'btn-primary':'btn'}
       onClick={this.handleClick}>{this.state.active?"Wyłącz": "Włącz"}</button>
     </div>
     );
