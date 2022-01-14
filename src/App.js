e9f7b2758e97f424420dd25754bad81f6b7ae4ea
Walachooo <j.walach@protonmail.com>
2022-01-14 13:57:41 +0100

    state in stateless functions via hooks

--- src/App.js
+++ src/App.js
@@ -2,43 +2,22 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-class App extends React.Component {
-  state={
-    active:true
-  }
-  handleClick = ()=>{
+const App = () => {
+  const [value, setValue] = React.useState(true);
+
+  const handleClick = ()=>{
  
-    this.setState({
-      active: !this.state.active
-    })
+    setValue(!value);
   }
-  render() { 
-    // const btn_off = {
-    //   border: '2px solid green',
-    //   padding: '10px 20px',
-    //   fontFamily : 'arial',
-    //   display:'block',
-    //   margin:'20px auto',
-    //   backgroundColor: 'white',
-    //   color: 'blue'
-    // }
-    // const btn_on = {
-    //   border: '4px solid blue',
-    //   padding: '10px 20px',
-    //   fontFamily : 'arial',
-    //   display:'block',
-    //   margin:'20px auto',
-    //   backgroundColor: 'red'
-    // }
-    
+  
     return (
     <div>
       <button 
-      className={this.state.active? 'btn-primary':'btn'}
-      onClick={this.handleClick}>{this.state.active?"Wyłącz": "Włącz"}</button>
+      className={value? 'btn-primary':'btn'}
+      onClick={handleClick}>{value?"Wyłącz": "Włącz"}</button>
     </div>
     );
-  }
+  
 }
  
 export default App;
